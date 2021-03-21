067017e26e02fc5d6a40ae74e17a0b95eeae6524
Zeke <51855202+zyrrus@users.noreply.github.com>
2021-03-21 10:39:49 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,5 @@
-import projects.fourier.FourierDisplay;
-import projects.fourier.OrderPointsDisplay;
-
 public class Main {
     public static void main(String[] args) {
-//        FourierDisplay fd = new FourierDisplay(600, 600);
-//        OrderPointsDisplay opd = new OrderPointsDisplay(600, 600, 60);
+        TestDisplay td = new TestDisplay(600, 600);
     }
 }
